1b85b6011d82f4d5054ae8d7afdb826a45c1ecd4
Tony Zhova <zhova24@gmail.com>
2021-03-03 17:54:51 +0200

    modified SearchPanel to class component

--- src/components/SearchPanel/SearchPanel.js
+++ src/components/SearchPanel/SearchPanel.js
@@ -1,21 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
+
 import "./SearchPanel.sass";
 
 import ItemStatusFilter from "../ItemStatusFilter/ItemStatusFilter";
 
-const SearchPanel = ({ filter }) => {
-  const searchText = "search";
-  const searchStyle = { fontSize: "15px" };
-  return (
-    <div className='SearchPanel'>
-      <input
-        className='search defaultInput'
-        style={searchStyle}
-        placeholder={searchText}
-      />
-      <ItemStatusFilter />
-    </div>
-  );
-};
-
-export default SearchPanel;
+export default class SearchPanel extends Component {
+  render() {
+    return (
+      <div className='SearchPanel'>
+        <input
+          className='search defaultInput'
+          placeholder='Search'
+        />
+        <ItemStatusFilter />
+      </div>
+    );
+  }
+}
